c3f5876b03b88666933ac6f582ebd7b25c3f51f1
Jiří Lopatář <github@lopatar.me>
2022-08-22 14:05:39 +0200

    fix cron script

--- app/cron/sys_accounts.php
+++ app/cron/sys_accounts.php
@@ -1,9 +1,13 @@
 <?php
 declare(strict_types=1);
 
+use app\config;
+use app\database\connection;
 use app\models\mail_account;
 
 require_once __DIR__ . '/../../vendor/autoload.php';
+
+connection::init(config::MYSQL_HOST, config::MYSQL_USER, config::MYSQL_PASS, config::MYSQL_DB);
 
 $accounts = mail_account::get_all();
 
