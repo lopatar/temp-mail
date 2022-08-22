a746e306190dfecd9e25f0e0e9dbe513a9e3f7c3
Jiří Lopatář <github@lopatar.me>
2022-08-22 13:57:29 +0200

    fix autoloading

--- public/index.php
+++ public/index.php
@@ -6,6 +6,8 @@ use app\routes;
 use sdk\app;
 use sdk\render\view;
 
+require __DIR__ . '/../vendor/autoload.php';
+
 view::set_default_path(__DIR__ . '/../app/views/');
 connection::init(config::MYSQL_HOST, config::MYSQL_USER, config::MYSQL_PASS, config::MYSQL_DB);
 
