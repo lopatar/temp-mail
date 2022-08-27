063d5895ca72dff14f1d00a8bbd56b627fc176a9
Jiří Lopatář <github@lopatar.me>
2022-08-27 11:11:57 +0200

    Use char instead of varchar

--- db.sql
+++ db.sql
@@ -1,6 +1,6 @@
 create table mail(
     id int not null auto_increment primary key,
-    username varchar(12) not null unique key,
-    password varchar(32) not null,
+    username char(12) not null unique key,
+    password char(32) not null,
     expires int(10) not null
 );
